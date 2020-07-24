a0f30d7ae2665d63a4265f9df9b508143ffd79ec
Paulina Hetman <info@pehaa.com>
2020-07-24 13:09:04 +0200

    Update script.js

--- alyragradients/js/script.js
+++ alyragradients/js/script.js
@@ -1,18 +1,19 @@
 "use strict"
 
-/* la variabtl gradients est chargé
-vous pouvez le voir dans la console
+/* 
+le script gradients.js est chargé avant script.js
+la variable gradients est disponible - vous pouvez le voir dans la console
 */
 console.log(gradients)
 
 /* dans le paragraphe #intro afficher la phrase 
 Voici une collection de ... dégradés prêts à utiliser dans vos feuilles de styles
 
-ou ... sont remplacer par le nombre de dégradés */
+ou ... correspond au nombre de dégradés */
 
 /* header : 
 ajouter la propriété backgroundImage au style de header
-ceci devrait devenir linear-gradient(90deg, .., ..)
+linear-gradient(90deg, .., ..)
 ou ... correspondent aux couleurs start et end d'un des objet de l'array gradients (choix aléatoire)
 
 Le bouton #header-button permet de regénérer le header (=changer le dégradé)
